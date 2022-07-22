256a5e61be57127f6fb1577246afd073e1ea0f82
wongsitu <sa_waika_4d_23@hotmail.com>
2022-07-21 21:31:57 -0500

    dialog fix 1

--- next.config.js
+++ next.config.js
@@ -9,10 +9,6 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  i18n: {
-    locales: ['en', 'es', 'cn'],
-    defaultLocale: 'en',
-  },
   images: {
     format: ['image/webp', 'image/png', 'image/jpeg'],
   },
